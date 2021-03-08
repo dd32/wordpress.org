9ec2777ea6c544a4223f5a537cb58c6ee1377e75
Dion Hulse <contact@dd32.id.au>
2021-03-08 13:31:02 +1000

    Migrate the Features endpoint to the REST API

--- wordpress.org/public_html/wp-content/plugins/theme-directory/rest-api/class-features-endpoint.php
+++ wordpress.org/public_html/wp-content/plugins/theme-directory/rest-api/class-features-endpoint.php
@@ -9,6 +9,14 @@ class Features_Endpoint {
 		$args = array(
 			'callback'            => array( $this, 'features' ),
 			'permission_callback' => '__return_true',
+			'args'                => [
+				'wp_version' => [
+					'default'           => 0,
+					'validate_callback' => function( $param ) {
+						return preg_match( '!^\d\.\d!', $param );
+					}
+				]
+			]
 		);
 
 		register_rest_route( 'themes/1.0', 'features', $args );
@@ -22,13 +30,149 @@ class Features_Endpoint {
 	 * @param \WP_REST_Request $request The Rest API Request.
 	 */
 	function features( $request ) {
-		$api = wporg_themes_query_api(
-			'feature_list',
-			$request->get_params(),
-			'api_object'
+		$wp_version = $request['wp_version'];
+
+		// Get version from user agent since it's not explicitly sent to feature_list requests in older API branches.
+		if (
+			// If WP_Version isn't set.
+			! $wp_version &&
+			// And it's an older API endpoint
+			( ! defined( 'THEMES_API_VERSION' ) || THEMES_API_VERSION <= 1.1 ) &&
+			// And it's a WordPress User Agent
+			preg_match( '|WordPress/([^;]+)|', $request->get_header( 'user_agent' ), $matches )
+		) {
+			$wp_version = $matches[1];
+		}
+
+		$tags = array(
+			__( 'Colors' )   => array(
+				'black'  => __( 'Black' ),
+				'blue'   => __( 'Blue' ),
+				'brown'  => __( 'Brown' ),
+				'gray'   => __( 'Gray' ),
+				'green'  => __( 'Green' ),
+				'orange' => __( 'Orange' ),
+				'pink'   => __( 'Pink' ),
+				'purple' => __( 'Purple' ),
+				'red'    => __( 'Red' ),
+				'silver' => __( 'Silver' ),
+				'tan'    => __( 'Tan' ),
+				'white'  => __( 'White' ),
+				'yellow' => __( 'Yellow' ),
+				'dark'   => __( 'Dark' ),
+				'light'  => __( 'Light' ),
+			),
+			__( 'Columns' )  => array(
+				'one-column'    => __( 'One Column' ),
+				'two-columns'   => __( 'Two Columns' ),
+				'three-columns' => __( 'Three Columns' ),
+				'four-columns'  => __( 'Four Columns' ),
+				'left-sidebar'  => __( 'Left Sidebar' ),
+				'right-sidebar' => __( 'Right Sidebar' ),
+			),
+			__( 'Layout' )   => array(
+				'fixed-layout'      => __( 'Fixed Layout' ),
+				'fluid-layout'      => __( 'Fluid Layout' ),
+				'responsive-layout' => __( 'Responsive Layout' ),
+			),
+			__( 'Features' ) => array(
+				'accessibility-ready'   => __( 'Accessibility Ready' ),
+				'blavatar'              => __( 'Blavatar' ),
+				'buddypress'            => __( 'BuddyPress' ),
+				'custom-background'     => __( 'Custom Background' ),
+				'custom-colors'         => __( 'Custom Colors' ),
+				'custom-header'         => __( 'Custom Header' ),
+				'custom-menu'           => __( 'Custom Menu' ),
+				'editor-style'          => __( 'Editor Style' ),
+				'featured-image-header' => __( 'Featured Image Header' ),
+				'featured-images'       => __( 'Featured Images' ),
+				'flexible-header'       => __( 'Flexible Header' ),
+				'front-page-post-form'  => __( 'Front Page Posting' ),
+				'full-width-template'   => __( 'Full Width Template' ),
+				'microformats'          => __( 'Microformats' ),
+				'post-formats'          => __( 'Post Formats' ),
+				'rtl-language-support'  => __( 'RTL Language Support' ),
+				'sticky-post'           => __( 'Sticky Post' ),
+				'theme-options'         => __( 'Theme Options' ),
+				'threaded-comments'     => __( 'Threaded Comments' ),
+				'translation-ready'     => __( 'Translation Ready' ),
+			),
+			__( 'Subject' )  => array(
+				'holiday'       => __( 'Holiday' ),
+				'photoblogging' => __( 'Photoblogging' ),
+				'seasonal'      => __( 'Seasonal' ),
+			)
 		);
 
-		return $api->get_result( 'raw' );
+		// Pre 3.8 installs get width tags instead of layout tags.
+		if ( $wp_version && version_compare( $wp_version, '3.7.999', '<' ) ) {
+			unset( $tags[ __( 'Layout' ) ] );
+			$tags[ __( 'Width' ) ] = array(
+				'fixed-width'    => __( 'Fixed Width' ),
+				'flexible-width' => __( 'Flexible Width' ),
+			);
+
+			if ( array_key_exists( 'accessibility-ready', $tags[ __( 'Features' ) ] ) ) {
+				unset( $tags[ __( 'Features' ) ]['accessibility-ready'] );
+			}
+		}
+
+		if ( ! $wp_version || version_compare( $wp_version, '3.9-beta', '>' ) ) {
+			$tags[ __( 'Layout' ) ] = array_merge( $tags[ __( 'Layout' ) ], $tags[ __( 'Columns' ) ] );
+			unset( $tags[ __( 'Columns' ) ] );
+		}
+
+		// See https://core.trac.wordpress.org/ticket/33407.
+		if ( ! $wp_version || version_compare( $wp_version, '4.6-alpha', '>' ) ) {
+			unset( $tags[ __( 'Colors' ) ] );
+			$tags[ __( 'Layout' ) ] = array(
+				'grid-layout'   => __( 'Grid Layout' ),
+				'one-column'    => __( 'One Column' ),
+				'two-columns'   => __( 'Two Columns' ),
+				'three-columns' => __( 'Three Columns' ),
+				'four-columns'  => __( 'Four Columns' ),
+				'left-sidebar'  => __( 'Left Sidebar' ),
+				'right-sidebar' => __( 'Right Sidebar' ),
+			);
+
+			unset( $tags[ __( 'Features' ) ]['blavatar'] );
+			$tags[ __( 'Features' ) ]['footer-widgets'] = __( 'Footer Widgets' );
+			$tags[ __( 'Features' ) ]['custom-logo']    = __( 'Custom Logo' );
+			asort( $tags[ __( 'Features' ) ] ); // To move footer-widgets to the right place.
+
+			$tags[ __( 'Subject' ) ] = array(
+				'blog'           => __( 'Blog' ),
+				'e-commerce'     => __( 'E-Commerce' ),
+				'education'      => __( 'Education' ),
+				'entertainment'  => __( 'Entertainment' ),
+				'food-and-drink' => __( 'Food & Drink' ),
+				'holiday'        => __( 'Holiday' ),
+				'news'           => __( 'News' ),
+				'photography'    => __( 'Photography' ),
+				'portfolio'      => __( 'Portfolio' ),
+			);
+		}
+
+		// See https://core.trac.wordpress.org/ticket/46272.
+		if ( ! $wp_version || version_compare( $wp_version, '5.2-alpha', '>=' ) ) {
+			$tags[ __( 'Layout' ) ]['wide-blocks']    = __( 'Wide Blocks' );
+			$tags[ __( 'Features' ) ]['block-styles'] = __( 'Block Editor Styles' );
+			asort( $tags[ __( 'Features' ) ] ); // To move block-styles to the right place.
+		}
+
+		// See https://core.trac.wordpress.org/ticket/50164.
+		if ( ! $wp_version || version_compare( $wp_version, '5.5-alpha', '>=' ) ) {
+			$tags[ __( 'Features' ) ]['block-patterns']    = __( 'Block Editor Patterns' );
+			$tags[ __( 'Features' ) ]['full-site-editing'] = __( 'Full Site Editing' );
+			asort( $tags[ __( 'Features' ) ] );
+		}
+
+		// Only return tag slugs, to stay compatible with bbpress-version of Themes API.
+		foreach ( $tags as $title => $group ) {
+			$tags[ $title ] = array_keys( $group );
+		}
+
+		return $tags;
 	}
 
 }
